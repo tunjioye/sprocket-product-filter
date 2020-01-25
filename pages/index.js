1ab019612825df75500fc08bd6ee287268991fd7
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-01-26 00:22:10 +0100

    UPDATE : search query working

--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,109 @@ import {
   Button,
   List,
   FlexboxGrid,
-  Col
+  Col,
+  Icon
 } from 'rsuite'
-import data from '../data'
+// import data from '../data'
 import ProductList from '../components/ProductList'
+import axios from 'axios'
+import { API_URL } from '../config'
 
 class IndexPage extends React.Component {
-  static getInitialProps () {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ...props,
+      searchQuery: ''
+    }
+
+    this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this)
+    this.handleSearchButtonClick = this.handleSearchButtonClick.bind(this)
+  }
+
+  static async getInitialProps () {
+    let products = []
+    let finalErrorMessage = ''
+
+    try{
+      const queryUrl = `${API_URL}/products`
+      const { data } = await axios.get(queryUrl, {
+        headers: {
+          'Access-Control-Allow-Origin': `*`
+        }
+      })
+
+      if (Array.isArray(data)) {
+        products = data
+      }
+    } catch (error) {
+      let errorMessage = 'Network Error! Please connect to the internet and try again.'
+
+      if (error.response) {
+        const { status } = error.response
+        if (status && status >= 500) {
+          errorMessage = 'Error! Something went wrong. Please try again later.'
+        }
+      }
+
+      finalErrorMessage = errorMessage
+    }
+
     return {
-      products: data
+      products,
+      errorMessage: finalErrorMessage
+    }
+  }
+
+  handleSearchQueryChange (value) {
+    this.setState({
+      searchQuery: value
+    })
+  }
+
+  async handleSearchButtonClick () {
+    const { searchQuery } = this.state
+
+    try{
+      const queryUrl = `${API_URL}/products/search?query=${searchQuery}`
+      const { data } = await axios.get(queryUrl, {
+        headers: {
+          'Access-Control-Allow-Origin': `*`
+        }
+      })
+
+      if (Array.isArray(data)) {
+        this.setState({
+          products: data
+        })
+      }
+    } catch (error) {
+      let errorMessage = 'Network Error! Please connect to the internet and try again.'
+
+      if (error.response) {
+        const { status } = error.response
+        if (status && status >= 500) {
+          errorMessage = 'Error! Something went wrong. Please try again later.'
+        }
+      }
+
+      alert(errorMessage)
+    }
+  }
+
+  componentDidMount () {
+    const { errorMessage } = this.props
+    if (errorMessage) {
+      alert(errorMessage)
     }
   }
 
   render() {
-    const { products } = this.props
+    const { products } = this.state
     const mappedProducts = products.map((product, productIndex) => (
       <ProductList product={product} productIndex={productIndex} key={productIndex} />
     ))
+    const { searchQuery } = this.state
 
     return (
       <>
@@ -35,9 +121,9 @@ class IndexPage extends React.Component {
           <Form layout="inline">
             <FormGroup className="mb--0">
               {/* <ControlLabel>Search Products</ControlLabel> */}
-              <FormControl placeholder="Enter Product" name="product_title" />
+              <FormControl placeholder="Enter Product" name="searchQuery" value={searchQuery} onChange={this.handleSearchQueryChange} />
             </FormGroup>
-            <Button appearance="primary" className="mb--0">Search</Button>
+            <Button appearance="primary" className="mb--0" onClick={this.handleSearchButtonClick}>Search</Button>
           </Form>
         </section>
         <section className="section section--sm">
@@ -45,9 +131,19 @@ class IndexPage extends React.Component {
           <div className="responsive--wrapper">
             <FlexboxGrid justify="center">
               <FlexboxGrid.Item componentClass={Col} colspan={24} md={18}>
-                <List hover>
-                  {mappedProducts}
-                </List>
+                {products.length > 0
+                  ? (
+                    <List hover>
+                      {mappedProducts}
+                    </List>
+                  )
+                  : (
+                    <div className="section bg--light align--center text--dark">
+                      <Icon className="mb--1" icon="ban" size="3x" style={{ color: '#f44336' }} />
+                      <p className="font__size--xlarge">No Products Found</p>
+                    </div>
+                  )
+                }
               </FlexboxGrid.Item>
             </FlexboxGrid>
           </div>
