a0662e4c92812391654b6cded7eea7425fb74698
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-01-27 16:16:09 +0100

    FIX : deployment to heroku

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import React from 'react'
 import withReduxStore from '../lib/with-redux-store'
 import { Provider } from 'react-redux'
 import 'rsuite/lib/styles/index.less'
-import '../assets/css/index.css'
+import '../assets/scss/index.scss'
 
 class MyApp extends App {
   render () {
